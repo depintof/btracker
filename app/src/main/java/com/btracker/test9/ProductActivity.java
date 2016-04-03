e600b6933af295ac477e518d452a045dab23a67b
Alejandro <alejandro.florezm@hotmail.com>
2016-04-03 17:29:44 -0500

    Alejo alejandro.florezm@hotmail.com
    *Product gallery
    *Navigation bar names

--- app/src/main/java/com/btracker/test9/ProductActivity.java
+++ app/src/main/java/com/btracker/test9/ProductActivity.java
@@ -2,12 +2,16 @@ package com.btracker.test9;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Paint;
+import android.net.Uri;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,17 +25,31 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class ProductActivity extends AppCompatActivity {
 
-    private Integer images[] = {R.drawable.pic1, R.drawable.pic2, R.drawable.pic3,R.drawable.pic4};
+    //private Integer images[] = {R.drawable.gafas, R.drawable.gorra, R.drawable.pantalon,R.drawable.pic4};
 
     TextView tvProducto;
     TextView tvDescripcion;
     TextView tvPrecioConDescuento;
     TextView tvPrecioOriginal;
     TextView tvDescuento;
+    ImageView displayImage;
+    LinearLayout myGallery;
     MenuItem favoriteMenu;
     boolean favoriteFlag;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @SuppressWarnings("deprecation")
     @Override
@@ -43,32 +61,52 @@ public class ProductActivity extends AppCompatActivity {
         tvProducto = (TextView) findViewById(R.id.producto);
         tvDescripcion = (TextView) findViewById(R.id.descripcion);
         tvPrecioConDescuento = (TextView) findViewById(R.id.precioConDescuento);
-        tvPrecioOriginal = (TextView)findViewById(R.id.precioOriginal);
+        tvPrecioOriginal = (TextView) findViewById(R.id.precioOriginal);
         tvDescuento = (TextView) findViewById(R.id.descuento);
+
+        displayImage = (ImageView) findViewById(R.id.productImage);
+        myGallery = (LinearLayout) findViewById(R.id.myGallery);
 
         tvPrecioOriginal.setPaintFlags(tvPrecioOriginal.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
 
         setToolbar();
 
-        // Note that Gallery view is deprecated in Android 4.1---
-        Gallery gallery = (Gallery) findViewById(R.id.myGallery);
-        if (gallery != null) {
-            gallery.setAdapter(new ImageAdapter(this));
-            gallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                    Toast.makeText(getBaseContext(), "pic" + (position + 1) + " selected",
-                            Toast.LENGTH_SHORT).show();
-                    // display the images selected
-                    ImageView imageView = (ImageView) findViewById(R.id.productImage);
-                    if (imageView != null) {
-                        imageView.setImageResource(images[position]);
+        //setGallery();
+
+        try {
+            String galleryDirectoryName = "gallery";
+            String[] listImages = getAssets().list(galleryDirectoryName);
+            for (String imageName : listImages) {
+                InputStream is = getAssets().open(galleryDirectoryName + "/" + imageName);
+                final Bitmap bitmap = BitmapFactory.decodeStream(is);
+
+                ImageView imageView = new ImageView(getApplicationContext());
+                imageView.setImageBitmap(bitmap);
+                imageView.setLayoutParams(new ViewGroup.LayoutParams(400, 400));
+                //imageView.setLayoutParams(new Gallery.LayoutParams(Gallery.LayoutParams.MATCH_PARENT, Gallery.LayoutParams.MATCH_PARENT));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                //imageView.setAdjustViewBounds(true);
+                //imageView.setMaxHeight(0);
+                imageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        displayImage.setImageBitmap(bitmap);
                     }
-                }
-            });
+                });
+
+                myGallery.addView(imageView);
+            }
+        } catch (IOException e) {
+            Log.e("GalleryScrollView", e.getMessage(), e);
         }
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public class ImageAdapter extends BaseAdapter {
+    /*public class ImageAdapter extends BaseAdapter {
         private Context context;
         private int itemBackground;
         public ImageAdapter(Context c)
@@ -99,10 +137,30 @@ public class ProductActivity extends AppCompatActivity {
             imageView.setLayoutParams(new Gallery.LayoutParams(Gallery.LayoutParams.WRAP_CONTENT, Gallery.LayoutParams.MATCH_PARENT));
             imageView.setAdjustViewBounds(true);
             imageView.setMaxHeight(0);
-            imageView.setBackgroundResource(itemBackground);
+            //imageView.setBackgroundResource(itemBackground);
             return imageView;
         }
     }
+
+    @SuppressWarnings("deprecation")
+    private void setGallery(){
+        // Note that Gallery view is deprecated in Android 4.1---
+        Gallery gallery = (Gallery) findViewById(R.id.myGallery);
+        if (gallery != null) {
+            gallery.setAdapter(new ImageAdapter(this));
+            gallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                    Toast.makeText(getBaseContext(), "pic" + (position + 1) + " selected",
+                            Toast.LENGTH_SHORT).show();
+                    // display the images selected
+                    ImageView imageView = (ImageView) findViewById(R.id.productImage);
+                    if (imageView != null) {
+                        imageView.setImageResource(images[position]);
+                    }
+                }
+            });
+        }
+    }*/
 
     private void setToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -131,7 +189,7 @@ public class ProductActivity extends AppCompatActivity {
                 return true;
             case R.id.product_like:
                 // app icon in action bar clicked; goto parent activity.
-                if(!favoriteFlag){
+                if (!favoriteFlag) {
                     favoriteMenu.setIcon(R.drawable.ic_favorite_pressed);
                     favoriteFlag = true;
                 } else {
@@ -144,4 +202,43 @@ public class ProductActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Product Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.btracker.test9/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Product Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.btracker.test9/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
