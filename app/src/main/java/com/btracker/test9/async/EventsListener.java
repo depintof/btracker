27557f0cb2016c3a821ae08c8d66104de05346a4
David <dapin146@gmail.com>
2016-04-03 20:49:06 -0500

    Database Connectivity - 2016-04-03

---
+++ app/src/main/java/com/btracker/test9/async/EventsListener.java
@@ -0,0 +1,12 @@
+package com.btracker.test9.async;
+
+import com.btracker.test9.dto.Beacon;
+
+import org.json.JSONObject;
+
+/**
+ * Events Generation
+ */
+public interface EventsListener {
+    void beaconsResult(JSONObject jsonResult);
+}
