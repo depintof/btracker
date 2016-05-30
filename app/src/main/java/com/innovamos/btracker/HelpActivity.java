ddeddcea04e31ade3ab071124da809e2b657f58a
Sergio <sdtorresl@unal.edu.co>
2016-05-30 12:46:47 -0500

    Improvements on help activity

--- app/src/main/java/com/innovamos/btracker/HelpActivity.java
+++ app/src/main/java/com/innovamos/btracker/HelpActivity.java
@@ -3,19 +3,32 @@ package com.innovamos.btracker;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.innovamos.btracker.utils.PagerAdapter;
 
-public class HelpActivity extends FragmentActivity {
+public class HelpActivity extends AppCompatActivity {
 
     ViewPager mViewPager;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            // Poner ícono y logo del drawer toggle
+            ab.setDisplayHomeAsUpEnabled(true);
+            ab.setTitle(R.string.title_activity_help);
+        }
 
         mViewPager = (ViewPager) findViewById(R.id.pager);
         PagerAdapter pAdapter = new PagerAdapter(getSupportFragmentManager());
@@ -26,20 +39,18 @@ public class HelpActivity extends FragmentActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_help, menu);
+        //getMenuInflater().inflate(R.menu.menu_help, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // If App icon in action bar clicked; goto parent activity.
+                this.finish();
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/innovamos/btracker/utils/MyAdapter.java
+++
@@ -1,7 +0,0 @@
-package com.innovamos.btracker.utils;
-
-/**
- * Created by sergio on 5/30/16.
- */
-public class MyAdapter {
-}
